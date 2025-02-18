429473e0614face8ad5be0a0ce1176e14d5a9127
salvadorrus <ntvkras@list.ru>
2025-02-18 23:44:14 +0300

    Fix workflows

---
+++ app/src/main/java/hexlet/code/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package hexlet.code.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+public class JacksonConfig {
+    @Bean
+    Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        var builder = new Jackson2ObjectMapperBuilder();
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL)
+                .modulesToInstall(new JsonNullableModule());
+        return builder;
+    }
+}
