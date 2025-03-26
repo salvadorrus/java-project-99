d072ad85a5f019cc417066d4d361368564d9be76
salvadorrus <ntvkras@list.ru>
2025-03-26 17:42:56 +0300

    Fix Dto (Task)

--- src/main/java/hexlet/code/specification/TaskSpecification.java
+++ src/main/java/hexlet/code/specification/TaskSpecification.java
@@ -11,8 +11,8 @@ public class TaskSpecification {
     public Specification<Task> build(TaskParamsDTO params) {
         return withTitleCont(params.getTitleCont())
                 .and(withAssigneeId(params.getAssigneeId()))
-                .and(withStatus(params.getStatus()))
-                .and(withLabelId(params.getLabelId()));
+                .and(withStatus(params.getStatus()));
+//                .and(withLabelId(params.getLabelId()));
     }
 
     private Specification<Task> withTitleCont(String titleCont) {
@@ -30,8 +30,8 @@ public class TaskSpecification {
                 : cb.equal(root.get("taskStatus").get("slug"), status);
     }
 
-    private Specification<Task> withLabelId(Long labelId) {
-        return (root, query, cb) -> labelId == null
-                ? cb.conjunction() : cb.equal(root.get("labels").get("id"), labelId);
-    }
+//    private Specification<Task> withLabelId(Long labelId) {
+//        return (root, query, cb) -> labelId == null
+//                ? cb.conjunction() : cb.equal(root.get("labels").get("id"), labelId);
+//    }
 }
